f548ba0ef9818b84ae945ee50a479e0d61170dd8
Roman Parker <roman@parker.pm>
2025-02-12 21:03:33 +0100

    experimented with parsing#

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 
 use std::collections::HashMap;
 use text_io::read;
+use chrono::NaiveDate;
 
 
 
@@ -20,16 +21,23 @@ fn main() {
         (6, "Friday"),
     ]);
 
-    let mut h: i32;
-    let q: i32 = 1;
+    // let mut h: i32;
+    // let q: i32 = 1;
     let user_date = get_user_input();
+    let user_date_parsed = get_date_from_input(user_date);
+
+}
+
+fn get_date_from_input(input: String) -> NaiveDate {
+    let parsed_date: NaiveDate = NaiveDate::parse_from_str(&input, "%Y-%m-%d").expect("Failed to parse date, enter date in format YYYY-MM-DD");
+    parsed_date
 }
 
 
 
-// fn adjust_for_leap_year() {
+fn adjust_for_leap_year() {
 
-// }
+}
 
 fn get_user_input() -> String {
     println!("Enter date");
